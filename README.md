d9ccab1b72321119f9c67e9d04261ac46c9e4642
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2020-03-24 16:07:35 -0400

    Added a custom strategy and a localized precache strategy

--- README.md
+++ README.md
@@ -21,14 +21,57 @@ Let's assume that your node is at `/content/myapp/us/en/sw`. In your page JavaSc
 
 ```js
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/content/myapp/us/en/sw');
+  navigator.serviceWorker.register('/content/myapp/us/en/sw.js');
 }
 ```
 
 ## Configuration
 
+The service worker resource needs a string property named 'strategy' with one of the following values:
 
-On your service worker resource you can add the following properties:
+1. `precache`
+2. `localizedPrecache`    
+3. `custom`
 
-1. A string property with the name 'version' and some value. This will be used to name the service worker cache.
-2. A multi valued string property with the name 'precacheFiles'. This list of urls will be cached when the service worker is installed.
+### Precache strategy
+
+If you are using the precache strategy you will also need to set the following properties:
+
+1. `version`: This should be the new version of the service worker.
+1. `files`: This should be a multi valued string property with the urls that you want included in the precache.
+
+### Localized precache strategy
+
+This strategy is good if you want to configure the service worker once and use it on a large number of localized country and language sites.
+
+If you are using the localized precache strategy you will also need to set the following properties:
+
+1. `version`: This should be the new version of the service worker.
+1. `startSegment`: This should be an integer containing the number of path segments that you want to ignore, starting from the root.
+1. `prefixLength`: This should be an integer containing the number of path segments that you want to use as a prefix, starting from the ignored depth. This value should be 1 or greater.
+1. `prefixedFiles`: This should be a multi valued string property with urls that you want included in the precache. These files will be prefixed by the calculated prefix. This value should be 1 or greater.
+1. `files`: This should be a multi valued string property with the urls that you want included in the precache. These files will not be prefixed.
+
+#### Example
+
+Path of the the service worker resource: `/content/mysite/us/en/sw.js`;
+
+```
+ignoreDepth=2
+prefixDepth=2
+prefixedFiles=["/home.html", "/offline.html", "/news/newsfeed.html"]
+```
+
+This will ignore the first 2 path sections, `/content/mysite`, and use the next 2 as a prefix, `/us/en`. Using this calculated prefix the following urls will be added to the precache:
+
+1. `/us/en/home.html`
+1. `/us/en/offline.html`
+1. `/us/en/news/newsfeed.html`
+
+### Custom strategy
+
+If you are using the custom strategy you need to set a property named `template` which contains the JavaScript of your service worker.
+
+Any properties on the service worker resource that are surrounded with `___` will be used to replace the contents of the custom template.
+
+For example if your custom template has `___EXAMPLE___` and your service worker resource has a property named `___EXAMPLE___` with the value `test`, your service worker template will have all instances of `___EXAMPLE___` replaced with `test`. These must be string properties.
--- core/src/main/java/com/aemserviceworker/servlets/ServiceWorkerServlet.java
+++ core/src/main/java/com/aemserviceworker/servlets/ServiceWorkerServlet.java
@@ -1,5 +1,6 @@
 package com.aemserviceworker.servlets;
 
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.servlets.ServletResolverConstants;
 import org.osgi.service.component.annotations.Component;
 import org.apache.sling.api.SlingHttpServletRequest;
@@ -10,9 +11,11 @@ import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.List;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.List;
 
 @Component(service=Servlet.class,
         property={
@@ -25,26 +28,101 @@ public class ServiceWorkerServlet extends SlingAllMethodsServlet {
     private static final long serialVersionUID = 2598426539166789515L;
     private static final Logger LOG = LoggerFactory.getLogger(ServiceWorkerServlet.class);
 
-    private static final String PROP_VERSION = "version";
-    private static final String DEFAULT_VERSION = "precache_v1";
-    private static final String CONFIG_VERSION = "___VERSION___";
-    private static final String PROP_PRECACHE_FILES = "precacheFiles";
-    private static final String[] DEFAULT_PRECACHE_FILES = new String[0];
-    private static final String CONFIG_PRECACHE_FILES = "___PRECACHE_FILES___";
-    private static final String STRATEGY_PRECACHE = "const PRECACHE='___VERSION___';const PRECACHE_URLS=[___PRECACHE_FILES___];self.addEventListener('install',event=>{event.waitUntil(caches.open(PRECACHE).then(cache=>cache.addAll(PRECACHE_URLS)).then(self.skipWaiting()))});self.addEventListener('activate',event=>{const currentCaches=[PRECACHE];event.waitUntil(caches.keys().then(cacheNames=>{return cacheNames.filter(cacheName=>!currentCaches.includes(cacheName))}).then(cachesToDelete=>{return Promise.all(cachesToDelete.map(cacheToDelete=>{return caches.delete(cacheToDelete)}))}).then(()=>self.clients.claim()))});self.addEventListener('fetch',event=>{if(event.request.url.startsWith(self.location.origin)){event.respondWith(caches.match(event.request).then(cachedResponse=>{if(cachedResponse){return cachedResponse}return fetch(event.request)}))}})";
+    private static final String CONFIG_BORDER = "___";
+    private static final String CONFIG_VERSION = "version";
+    private static final String CONFIG_PRECACHE_FILES = "files";
+    private static final String CONFIG_PREFIXED_PRECACHE_FILES = "prefixedFiles";
+    private static final String CONFIG_IGNORE_DEPTH = "ignoreDepth";
+    private static final String CONFIG_PREFIX_DEPTH = "prefixDepth";
+    private static final String PROP_CUSTOM_TEMPLATE = "template";
+    private static final String PROP_STRATEGY = "strategy";
+    private static final String STRATEGY_PRECACHE = "precache";
+    private static final String STRATEGY_LOCALIZED_PRECACHE = "localizedPrecache";
+    private static final String STRATEGY_CUSTOM = "custom";
+    private static final String SW_PRECACHE = "const PRECACHE='___version___';const PRECACHE_URLS=[___files___];self.addEventListener('install',event=>{event.waitUntil(caches.open(PRECACHE).then(cache=>cache.addAll(PRECACHE_URLS)).then(self.skipWaiting()))});self.addEventListener('activate',event=>{const currentCaches=[PRECACHE];event.waitUntil(caches.keys().then(cacheNames=>{return cacheNames.filter(cacheName=>!currentCaches.includes(cacheName))}).then(cachesToDelete=>{return Promise.all(cachesToDelete.map(cacheToDelete=>{return caches.delete(cacheToDelete)}))}).then(()=>self.clients.claim()))});self.addEventListener('fetch',event=>{if(event.request.url.startsWith(self.location.origin)){event.respondWith(caches.match(event.request).then(cachedResponse=>{if(cachedResponse){return cachedResponse}return fetch(event.request)}))}})";
 
     @Override
     protected void doGet(final SlingHttpServletRequest request, final SlingHttpServletResponse response) {
         try {
-            final String version = request.getResource().getValueMap().get(PROP_VERSION, DEFAULT_VERSION);
-            final String precacheFiles = Arrays.asList(request.getResource().getValueMap().get(PROP_PRECACHE_FILES, DEFAULT_PRECACHE_FILES)).stream()
-                    .map(str -> "'" + str + "'").collect(Collectors.joining(","));
-            final String sw = STRATEGY_PRECACHE.replace(CONFIG_VERSION, version).replace(CONFIG_PRECACHE_FILES, precacheFiles);
+            final String strategy = request.getResource().getValueMap().get(PROP_STRATEGY, String.class);
+            String sw;
+
+            if (STRATEGY_PRECACHE.equals(strategy)) {
+                sw = precacheSw(request.getResource());
+            } else if (STRATEGY_LOCALIZED_PRECACHE.equals(strategy)) {
+                sw = localizedPrecacheSw(request.getResource());
+            } else if (STRATEGY_CUSTOM.equals(strategy)) {
+                sw = customSw(request.getResource());
+            } else {
+                throw new ServiceWorkerException("Service worker config at " + request.getResource().getPath() + " had no strategy set");
+            }
 
             response.setContentType("text/javascript");
             response.getWriter().write(sw);
         } catch(Exception e) {
             LOG.error("ServiceWorkerServlet error", e);
+        }
+    }
+
+    private String precacheSw(final Resource resource) {
+        if (resource.getValueMap().containsKey(CONFIG_VERSION) && resource.getValueMap().containsKey(CONFIG_PRECACHE_FILES)) {
+            final String version = resource.getValueMap().get(CONFIG_VERSION, String.class);
+            final String precacheFiles = Arrays.asList(resource.getValueMap().get(CONFIG_PRECACHE_FILES, String[].class)).stream()
+                    .map(str -> "'" + str + "'")
+                    .collect(Collectors.joining(","));
+
+            return SW_PRECACHE
+                    .replace(CONFIG_BORDER + CONFIG_VERSION + CONFIG_BORDER, version)
+                    .replace(CONFIG_BORDER + CONFIG_PRECACHE_FILES + CONFIG_BORDER, precacheFiles);
+        } else {
+            throw new ServiceWorkerException("Service worker config at " + resource.getPath() + " was set to precache strategy but is either missing " + CONFIG_VERSION + " or " + CONFIG_PRECACHE_FILES + ". Make sure this service worker config has all of these properties set.");
+        }
+    }
+
+    private String localizedPrecacheSw(final Resource resource) {
+        if (resource.getValueMap().containsKey(CONFIG_VERSION) && resource.getValueMap().containsKey(CONFIG_PRECACHE_FILES) &&
+                resource.getValueMap().containsKey(CONFIG_PREFIXED_PRECACHE_FILES) && resource.getValueMap().containsKey(CONFIG_IGNORE_DEPTH) &&
+                resource.getValueMap().containsKey(CONFIG_PREFIX_DEPTH)) {
+            final String version = resource.getValueMap().get(CONFIG_VERSION, String.class);
+            final Integer ignoreDepth = resource.getValueMap().get(CONFIG_IGNORE_DEPTH, Integer.class);
+            final Integer prefixDepth = resource.getValueMap().get(CONFIG_PREFIX_DEPTH, Integer.class);
+            final String prefix = "/" + Arrays.asList(Arrays.copyOfRange(resource.getPath().split("/"), ignoreDepth + 1, ignoreDepth + 1 + prefixDepth)).stream().collect(Collectors.joining("/"));
+            final List<String> precacheFiles = Arrays.asList(resource.getValueMap().get(CONFIG_PRECACHE_FILES, String[].class)).stream()
+                    .map(str -> "'" + str + "'").collect(Collectors.toList());
+            final List<String> localizedPrecacheFiles = Arrays.asList(resource.getValueMap().get(CONFIG_PREFIXED_PRECACHE_FILES, String[].class)).stream()
+                    .map(str -> "'" + prefix + str + "'").collect(Collectors.toList());
+
+            final List<String> files = new ArrayList<>();
+            files.addAll(precacheFiles);
+            files.addAll(localizedPrecacheFiles);
+
+            return SW_PRECACHE
+                    .replace(CONFIG_BORDER + CONFIG_VERSION + CONFIG_BORDER, version)
+                    .replace(CONFIG_BORDER + CONFIG_PRECACHE_FILES + CONFIG_BORDER, files.stream().collect(Collectors.joining(",")));
+        } else {
+            throw new ServiceWorkerException("Service worker config at " + resource.getPath() + " was set to precache strategy but is missing one of: " + CONFIG_VERSION + ", " + CONFIG_PRECACHE_FILES + ", " + CONFIG_IGNORE_DEPTH + ", " + CONFIG_PREFIX_DEPTH + ", " + CONFIG_PREFIXED_PRECACHE_FILES + ". Make sure this service worker config has all of these properties set.");
+        }
+    }
+
+    private String customSw(final Resource resource) {
+        if (resource.getValueMap().containsKey(PROP_CUSTOM_TEMPLATE)) {
+            String sw = resource.getValueMap().get(PROP_CUSTOM_TEMPLATE, String.class);
+
+            for (String config : resource.getValueMap().keySet()) {
+                if (config.startsWith(CONFIG_BORDER) && config.endsWith(CONFIG_BORDER)) {
+                    sw = sw.replace(config, resource.getValueMap().get(config, ""));
+                }
+            }
+
+            return sw;
+        } else {
+            throw new ServiceWorkerException("Service worker config at " + resource.getPath() + " was set to custom strategy but contained no custom template. Please add a custom service worker template to the " + PROP_CUSTOM_TEMPLATE + " variable.");
+        }
+    }
+
+    public class ServiceWorkerException extends RuntimeException {
+        public ServiceWorkerException(String errorMessage) {
+            super(errorMessage);
         }
     }
 }
