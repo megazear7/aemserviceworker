e026707df780dd3116f2a7d259af0ce82d3a59a3
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2020-03-24 14:20:23 -0400

    Added an actual service worker.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # AEM Service Worker
 
+This is a service worker generator for AEM. It allows you to place nodes with a specific resource type throughout your content hierarchy. When accessed with the '.js' extension these nodes will return a service worker based upon the properties on the node.
+
 ## Installation
+
+Either install the package to the CRX or install the project using the below command.
 
 ```
 mvn clean install
@@ -8,19 +12,23 @@ mvn clean install
 
 ## Usage
 
-Create a `sw.js` node under your content hierarchy with the following properties:
+Create a node under your content hierarchy with the following properties. This is your 'service worker resource'.
 
 1. `jcr:primaryType=nt:unstructured`
 1. `sling:resourceType=aemserviceworker/serviceworker`
 
-In your page JavaScript add the following:
+Let's assume that your node is at `/content/myapp/us/en/sw`. In your page JavaScript add the following:
 
 ```js
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/your/content/path/sw.js');
+  navigator.serviceWorker.register('/content/myapp/us/en/sw');
 }
 ```
 
 ## Configuration
 
-TODO Enable Workbox configuration of service worker.
+
+On your service worker resource you can add the following properties:
+
+1. A string property with the name 'version' and some value. This will be used to name the service worker cache.
+2. A multi valued string property with the name 'precacheFiles'. This list of urls will be cached when the service worker is installed.
--- core/src/main/java/com/aemserviceworker/servlets/ServiceWorkerServlet.java
+++ core/src/main/java/com/aemserviceworker/servlets/ServiceWorkerServlet.java
@@ -10,6 +10,9 @@ import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Component(service=Servlet.class,
         property={
@@ -22,11 +25,24 @@ public class ServiceWorkerServlet extends SlingAllMethodsServlet {
     private static final long serialVersionUID = 2598426539166789515L;
     private static final Logger LOG = LoggerFactory.getLogger(ServiceWorkerServlet.class);
 
+    private static final String PROP_VERSION = "version";
+    private static final String DEFAULT_VERSION = "precache_v1";
+    private static final String CONFIG_VERSION = "___VERSION___";
+    private static final String PROP_PRECACHE_FILES = "precacheFiles";
+    private static final String[] DEFAULT_PRECACHE_FILES = new String[0];
+    private static final String CONFIG_PRECACHE_FILES = "___PRECACHE_FILES___";
+    private static final String STRATEGY_PRECACHE = "const PRECACHE='___VERSION___';const PRECACHE_URLS=[___PRECACHE_FILES___];self.addEventListener('install',event=>{event.waitUntil(caches.open(PRECACHE).then(cache=>cache.addAll(PRECACHE_URLS)).then(self.skipWaiting()))});self.addEventListener('activate',event=>{const currentCaches=[PRECACHE];event.waitUntil(caches.keys().then(cacheNames=>{return cacheNames.filter(cacheName=>!currentCaches.includes(cacheName))}).then(cachesToDelete=>{return Promise.all(cachesToDelete.map(cacheToDelete=>{return caches.delete(cacheToDelete)}))}).then(()=>self.clients.claim()))});self.addEventListener('fetch',event=>{if(event.request.url.startsWith(self.location.origin)){event.respondWith(caches.match(event.request).then(cachedResponse=>{if(cachedResponse){return cachedResponse}return fetch(event.request)}))}})";
+
     @Override
     protected void doGet(final SlingHttpServletRequest request, final SlingHttpServletResponse response) {
         try {
+            final String version = request.getResource().getValueMap().get(PROP_VERSION, DEFAULT_VERSION);
+            final String precacheFiles = Arrays.asList(request.getResource().getValueMap().get(PROP_PRECACHE_FILES, DEFAULT_PRECACHE_FILES)).stream()
+                    .map(str -> "'" + str + "'").collect(Collectors.joining(","));
+            final String sw = STRATEGY_PRECACHE.replace(CONFIG_VERSION, version).replace(CONFIG_PRECACHE_FILES, precacheFiles);
+
             response.setContentType("text/javascript");
-            response.getWriter().write("console.log('Hello World!')");
+            response.getWriter().write(sw);
         } catch(Exception e) {
             LOG.error("ServiceWorkerServlet error", e);
         }
